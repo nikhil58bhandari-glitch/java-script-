4361cf4fe07a6995a4ca75326cf7323e9077e530
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-04-08 12:13:59 +0530

    js basics

---
+++ variables-data-types/console/script.js
@@ -0,0 +1,25 @@
+console.log(100);
+
+console.log('hello world');
+
+console.log(20, 'nikhil', true);
+
+const x = 100;
+
+console.log(x);
+
+console.error('Alert');
+
+console.warn('Warning');
+
+console.table({name: 'Nikhil', email: 'nikhil58bhandari@gmail.com'});
+
+console.group('simple');
+console.log(x);
+console.error('Alter');
+console.warn('Warning');
+console.groupEnd();
+
+const style = 'pandding: 10px; background-color: white; color: green';
+
+console.log('%chello world', style);
