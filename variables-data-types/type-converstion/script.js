268ec15400cc248078266cffb731857de4755722
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-04-13 10:49:08 +0530

    Type-Converstion

---
+++ variables-data-types/type-converstion/script.js
@@ -0,0 +1,26 @@
+// let amount = '1';
+// let amount = '99.5';  // for decimal
+let amount = 'hello';
+
+// Convert string to numbers
+amount = parseInt(amount);
+// amount = +amount;
+//  amount  = Number(amount);
+
+// Convert number to string
+// amount = amount.toString();
+//amount = String(amount);
+
+// Convert string to decimal
+// amount = parseFloat(amount);
+
+
+// Convert number to boolean
+// amount = Boolean(amount); // it give every time 'true' but putting 0 it give 'false' 
+
+console.log(Math.sqrt(-1));
+console.log(1 + NaN);
+console.log(undefined + undefined);
+console.log('foo' / 3);
+
+console.log(amount, typeof amount);
