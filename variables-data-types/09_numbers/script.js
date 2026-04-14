c8af25e531f11e9eb1469762e81a5ee5f0027b14
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-04-14 10:32:39 +0530

    Number and Math-Object

--- variables-data-types/09_numbers/script.js
+++ variables-data-types/09_numbers/script.js
@@ -1,3 +1,4 @@
+/*
 let x;
 
 const num = new Number(5.4267);
@@ -20,3 +21,37 @@ X = Number.MIN_VALUE;
 console.log(num)
  console.log(typeof x);
 console.log(x);
+*/
+
+
+
+
+ // MATH-OBJECTS
+
+ let x;
+
+ x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x = Math.round(4.2);
+
+x = Math.ceil(4.2);
+
+x = Math.floor(4.9);
+
+x = Math.pow(2, 3);
+
+x = Math.min(4, 5, 6);
+
+x = Math.max(4, 6, 5);
+
+x = Math.random();
+
+let y = Math.random() * 10 + 1;
+
+y = Math.floor(Math.random() * 100 + 1);
+
+console.log(Math);
+console.log(x);
+console.log(y);
