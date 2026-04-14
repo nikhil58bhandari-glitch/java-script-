c267fea29b1c8801bfb2f6c2168fda375e7b98c9
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-04-14 10:18:57 +0530

    Numbers

---
+++ variables-data-types/09_numbers/script.js
@@ -0,0 +1,22 @@
+let x;
+
+const num = new Number(5.4267);
+
+x = num.toString();
+
+x = num.toString().length;
+
+x = num.toFixed(2);
+
+x = num.toPrecision(3);
+
+x = num.toExponential(2);
+
+x = num.valueOf();
+
+x = Number.MAX_VALUE;
+X = Number.MIN_VALUE;
+
+console.log(num)
+ console.log(typeof x);
+console.log(x);
