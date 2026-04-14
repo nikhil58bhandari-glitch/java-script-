2ff055c1783b11b8338a43a66527c2d402c89c69
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-04-14 12:23:36 +0530

    Dates-Times and Methods

--- variables-data-types/10_dates_and_time/script.js
+++ variables-data-types/10_dates_and_time/script.js
@@ -1,3 +1,4 @@
+/*
 let d;
 
 d = new Date();
@@ -16,7 +17,53 @@ d = new Date();
 d = d.getTime();
 d = d.valueOf();
 
+
 d = Math.floor(Date.now() / 1000);
 
 console.log(typeof d);
 console.log(d);
+*/
+
+
+// Dates object method
+
+let x;
+let d = new Date();
+
+x = d.toString();
+
+x = d.getTime();
+x = d.valueOf();
+
+x = d.getFullYear();
+x = d.getMonth();
+x= d.getMonth() + 1;
+
+x = d.getDate();
+x = d.getDay();
+
+x = d.getHours();
+
+x = d.getSeconds();
+
+x = d.getMilliseconds();
+
+x = `${d.getFullYear()}-${d.getMonth() +1}-${d.getDate()}`;
+
+x = Intl.DateTimeFormat('en-US').format(d);
+x = Intl.DateTimeFormat('default').format(d);
+x = Intl.DateTimeFormat('defult', {month: 'long' }).format(d);
+x = d.toLocaleString('default', {month: 'long'});
+x = d.toLocaleString('default',{
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timezone: 'India/Uttrakhand',
+});
+
+
+console.log(x);
