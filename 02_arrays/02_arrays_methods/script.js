3271ea07baa59424dfad5154eb1b48cddb8d23d1
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-04-18 10:03:03 +0530

    Array Methods, Nesting, Nesting, Concat & Spread

--- 02_arrays/02_arrays_methods/script.js
+++ 02_arrays/02_arrays_methods/script.js
@@ -1,3 +1,4 @@
+/*
 let x;
 
 const  arr = [22, 23,43,5,4,65,34];
@@ -24,4 +25,49 @@ x = arr.splice(1, 4).reverse().toString().charAt(0);
 
 
 console.log(arr);
+console.log(x);
+*/
+
+//     Nesting, Concat & Spread
+
+let x;
+
+const fruits = ['apple', 'pear', 'banana', 'orange'];
+const berries = ['strawberry', 'blueberry' , 'raseberry'];
+
+//fruits.push(berries);
+
+// x = fruits[4][1];
+
+const allFruits = [fruits, berries];
+
+x = allFruits[1][0];
+
+x = fruits.concat(berries);
+
+// Spread Operator (...)-
+
+x = [...fruits, ...berries];
+
+// Flatten Arrays-
+
+const arr = [1,2,[3,4],5,[6,7],8];
+x = arr.flat();
+
+console.log(x);
+console.log(x);
+
+
+// Static Method on Array object 
+
+x = Array.isArray(fruits);
+x = Array.isArray('hello');
+
+x = Array.from('12345');
+
+const a = 1;
+const b  = 2;
+const c = 3;
+x = Array.of(a,b,c);
+
 console.log(x);
