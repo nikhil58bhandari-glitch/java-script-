172de1c8f719dc5f8f7116a2f10c7cc2788beb79
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-04-21 11:33:38 +0530

    Function Basics

---
+++ 03_functions/01_function_basic/script.js
@@ -0,0 +1,22 @@
+function sayHello(){
+    console.log('Hello world');
+}
+
+sayHello();
+
+function add (num1, num2){
+    console.log(num1 + num2);
+
+}
+add(5,8);
+
+function subtract(num1, num2){
+   // console.log(num1 - num2);
+
+return num1 - num2;
+console.log('After the return');
+
+}
+
+const result = subtract(34, 22);
+console.log(result,subtract(24,5));
