5a51f74dec156270f165f364e3a727ca6b2e63bb
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-04-23 10:09:46 +0530

    Global & Function Scope, Block Scope

--- 03_functions/03_global_function_scope/script.js
+++ 03_functions/03_global_function_scope/script.js
@@ -1,6 +1,6 @@
 // alert('Hello');
 // console.log(innerWidth);
-
+/*
 const x = 100;
 
 console.log(x, 'in global');
@@ -26,3 +26,40 @@ function add() {
 //console.log(y);
 
 add();
+*/
+
+//       Block Scope
+
+const x = 100;
+const foo = 1;
+var bar = 2;
+
+if(true) {
+    const y = 200;
+    console.log(x + y);
+}
+// console.log(x+y);
+
+for (let i = 0; i <= 10; i++){
+    console.log(i);
+}
+//  console.log(i);
+
+if(true){
+    const a = 500;
+    let b = 600;
+    var c = 700;
+}
+
+// console.log(a);        // it will give you error
+// console.log(b);        // it will also give you error
+console.log(c);        // it will not give a error becuse of var
+
+
+function run(){
+    var d = 100;
+    console.log(d);
+}
+run();
+
+//console.log(d);
