fcf1c30f22f201ccfc494b8ddee61cb4827ca76f
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-04-29 11:59:53 +0530

    switch statement

---
+++ 04_logic_control-flow/03_switch_statement/script.js
@@ -0,0 +1,53 @@
+const d = new Date(2026, 1, 10, 8, 0, 0);
+const month = d.getMonth();
+
+switch (month){
+    case 1:
+    console.log('It is january');
+    break;
+
+    case 2:
+        console.log('it is february');
+        break;
+
+        case 3:
+            console.log('it is march');
+            break;
+
+            case 4:
+            console.log('it is april');
+            break;
+
+            case 5:
+            console.log('it is may');
+            break;
+
+            case 6:
+            console.log('it is june');
+            break;
+
+            case 7:
+            console.log('it is july');
+            break;
+
+            case 8:
+            console.log('it is august');
+            break;
+
+            case 9:
+            console.log('it is september');
+            break;
+
+            case 10:
+            console.log('it is october');
+            break;
+
+            case 11:
+            console.log('it is november');
+            break;
+
+            case 12:
+            console.log('it is December');
+            break;
+
+}
