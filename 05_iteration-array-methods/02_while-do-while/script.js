167ea28c0d117c6bc94e59191826d442c27d235a
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-04-30 13:08:52 +0530

    While Loops & Do-While Loops

---
+++ 05_iteration-array-methods/02_while-do-while/script.js
@@ -0,0 +1,34 @@
+let i = 21;
+
+// while (i <= 20) {
+//     console.log('Number ' + i);
+//     i++;
+// }
+
+// loop over array
+
+const arr = [12,24,36,48,60];
+
+// while(i < arr.length) {
+//     console.log(arr[i]);
+//     i++;
+// }
+
+// Nesting while loops
+// while (i <= 5){
+//     console.log('Number' + i);
+
+//     let j = 1;
+//     while(j <= 5){
+//         console.log(`${i} * ${j} = ${i * j}`);
+//         j++;
+//     }
+//     i++;
+// }
+
+// do while loops 
+do {
+    console.log('Number '+ i);
+    i++;
+} while(i <= 20);
+
