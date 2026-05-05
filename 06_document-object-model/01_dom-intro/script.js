e0025a3d6a4680867b62344b6534a6555ba44e7a
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-05-05 12:08:38 +0530

    Documnet Object Model Intro

---
+++ 06_document-object-model/01_dom-intro/script.js
@@ -0,0 +1,16 @@
+// console.log(window.document);
+
+// console.log(document.body.innerText);
+
+// console.log(document.links[0]);
+
+// document.body.innerHTML = '<H1>Hello World</h1>';
+
+//document.write('Hello from js');
+
+console.log(document.getElementById('main'));
+
+const main = document.getElementById('main');
+// main.innerHTML = '<h1>hello from main</h1>';
+
+document.querySelector('#main h1').innerText = 'Hello';
