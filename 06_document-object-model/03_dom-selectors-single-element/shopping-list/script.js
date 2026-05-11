1342846e06fc30e5bae229d382b37e02e977025d
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-05-11 09:33:58 +0530

    Dom Selection Single Element

---
+++ 06_document-object-model/03_dom-selectors-single-element/shopping-list/script.js
@@ -0,0 +1,11 @@
+// document.getElementById()
+
+console.log(document.getElementById('app-title'));
+console.log(document.getElementById('app-title').id);
+console.log(document.getElementById('app-title').class);
+console.log(document.getElementById('app-title').className);
+console.log(document.getElementById('app-title').getAttribute('id'));
+
+// Set attributes
+
+document.getElementById('app-title').title = 'Shopping List';
